2fb23c2346e2902d0c1e7208919d1b71d078a196
kasampradeepti <37012072+kasampradeepti@users.noreply.github.com>
2023-07-01 20:20:10 +0530

    to displayEmployeeList from Database using HttpClient

---
+++ FirstApp/src/app/employee-list/employee-list.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Employee } from '../employee';
+import { EmployeeService } from '../employee.service';
+import { Observable } from 'rxjs';
+@Component({
+  selector: 'app-employee-list',
+  templateUrl: './employee-list.component.html',
+  styleUrls: ['./employee-list.component.css']
+})
+export class EmployeeListComponent implements OnInit {
+
+  employees: Employee[] = [];
+
+  constructor(private employeeService : EmployeeService) { }
+
+  ngOnInit() : void {
+    this.getEmployee();
+  }
+
+  private getEmployee() {
+ this.employeeService.getEmployees().subscribe( data => {
+  this.employees = data;
+});
+  }
+}
